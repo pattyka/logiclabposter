e5206c23ed54b371604c63f2ed3106adc918714f
Gábor Csaba <gaborcsabusz@gmail.com>
2026-04-09 19:26:12 +0200

    Update Firebase config to user's project

--- firebase-config.js
+++ firebase-config.js
@@ -3,13 +3,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBuB42gcJc_pWQQJ1Fy9OH3NgpFdj68R50",
-    authDomain: "logiclabwaitlist.firebaseapp.com",
-    projectId: "logiclabwaitlist",
-    storageBucket: "logiclabwaitlist.firebasestorage.app",
-    messagingSenderId: "220641691358",
-    appId: "1:220641691358:web:3541f84fda89298c2de44f",
-    measurementId: "G-NWKPV5GW2G"
+    apiKey: "AIzaSyDX_tUUYMvrUDZ_F_cbL716QCaKYJksmhQ",
+    authDomain: "logiclab-kids-290f5.firebaseapp.com",
+    projectId: "logiclab-kids-290f5",
+    storageBucket: "logiclab-kids-290f5.firebasestorage.app",
+    messagingSenderId: "490316514529",
+    appId: "1:490316514529:web:4502ab76f42db3b34163a0",
+    measurementId: "G-E8W35XPSZN"
 };
 
 // Initialize Firebase
